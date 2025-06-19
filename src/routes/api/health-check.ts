dd2950d9c5168c97ecf95f18436628b2a7d81abb
Caio Fernando <caiofsr@proton.me>
2025-06-19 00:55:30 -0300

    feat: add console log for health check route

--- src/routes/api/health-check.ts
+++ src/routes/api/health-check.ts
@@ -2,6 +2,7 @@ import { createServerFileRoute } from '@tanstack/react-start/server';
 
 export const ServerRoute = createServerFileRoute('/api/health-check').methods({
 	GET: async () => {
+		console.log('health checked');
 		return new Response(JSON.stringify({ status: 'ok' }), {
 			headers: {
 				'Content-Type': 'application/json',
